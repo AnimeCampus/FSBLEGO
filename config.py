a394e415f680e6fd43ac384c8b13d12ec7f11b6e
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-08-20 18:22:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -40,7 +40,7 @@ DB_URI = os.environ.get("DATABASE_URL", "postgres://dokpjiwg:fto8IbGuPeXhtwyF12_
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001884833556"))
-FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "0")) 
+FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1001884833556")) 
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
